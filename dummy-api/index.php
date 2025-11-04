10e44b4f59c5c7509384a2f91ac6fe77fa62a8f9
S Szabo <szilard.szabo@southeasternrailway.co.uk>
2025-11-04 12:42:27 +0000

    fix: - corrected results endpoint url in dummy api index - comment added to dummy-api index file stating stub nature of the api docs: - moved all docs and example files to a new separate docs folder

--- dummy-api/index.php
+++ dummy-api/index.php
@@ -4,11 +4,13 @@
 // GET  /api/result -> returns { sensor: 'temp', value: <random> }
 // POST /api/set    -> echoes back the json posted
 
+// !!! At the moment it is a stub only returning random values that does not reflect the real API behavior. !!!
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 header('Content-Type: application/json');
 
-if ($uri === '/api/result' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+if ($uri === '/api/results' && $_SERVER['REQUEST_METHOD'] === 'GET') {
     $value = round(20 + (mt_rand() / mt_getrandmax()) * 10, 2);
     echo json_encode(['sensor' => 'temp', 'value' => $value, 'ts' => time()]);
     exit(0);
