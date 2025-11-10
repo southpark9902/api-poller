6e3358009163223c6ce5a4fceda3d5c913934775
S Szabo <szilard.szabo@southeasternrailway.co.uk>
2025-11-10 16:00:05 +0000

    docs: - api endpoint access requirements returned by an api endpoint - test api call crafted using curl requests sent from powershell - questions sheet sent to siemens to clarifiy their DMS API - list of subsytems and their abbreviation - list of subsystems screenshot from DMS UI - comments added du,,y api router

--- dummy-api/router.php
+++ dummy-api/router.php
@@ -1,8 +1,11 @@
 <?php
 
-// To run this dummy API server locally on windows machine from power shell:
+// To run and test this dummy API server locally on windows machine from power shell:
 // from the folder C:\Users\sszabo, run the below command:
 // Start-Process php -ArgumentList '-S','localhost:8000','-t','"c:\Users\sszabo\OneDrive - Southeastern\Desktop\development\VEMS\api-poller\dummy-api"','"c:\Users\sszabo\OneDrive - Southeastern\Desktop\development\VEMS\api-poller\dummy-api\router.php"' -NoNewWindow -WorkingDirectory 'c:\Users\sszabo\OneDrive - Southeastern\Desktop\development\VEMS\api-poller'
+// Then to test run the below from another terminal to poll and endpoint:
+// make sure you are in this folder: C:\Users\sszabo\OneDrive - Southeastern\Desktop\development\VEMS\api-poller
+// and run: php .\api-pollers\timeseries-search.php
 
 // Simple router for PHP built-in server
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
